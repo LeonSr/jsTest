ab7de078d6b5f76a013335615b2d1531f9d2646a
sina-Ent <dev.sinarasooli@gmail.com>
2025-09-02 12:54:10 +0330

    view problem solution

---
+++ 4.API/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.set("view engine","ejs");
+
+
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname, "index"),{
+    sina: "sina"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is running in http://localhost:${port}`);
+});
